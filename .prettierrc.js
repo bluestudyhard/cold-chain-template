60e99db5fbfaa31364ed8ba8e69adf5671e4fc4f
bluestudyhard <3164752596@qq.com>
2024-06-14 16:16:13 +0800

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/** @type {import("prettier").Config} */
+export default {
+  bracketSpacing: true,
+  singleQuote: false,
+  arrowParens: "avoid",
+  trailingComma: "none",
+  // 没有分号
+  semi: false,
+  tabWidth: 4
+}
