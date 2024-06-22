854554454e30d4556725c4899c3dd78dc0e295c3
Stupidsheepy <2983626014@qq.com>
2024-06-22 15:15:41 +0800

    feat(amap): 添加amap地图实时位置

--- src/views/amap/index.vue
+++ src/views/amap/index.vue
@@ -1,8 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+// 传入起点终点和当前车位置
+const path = ref<[number, number][]>([
+  [116.5, 40],
+  [113.2, 24.1],
+])
+const carPosition = ref<[number, number]>([116.5, 40])
+</script>
 
 <template>
   <div id="container">
-    <amapContainer />
+    <amapContainer :path="path" :car-position="carPosition" />
   </div>
 </template>
 
